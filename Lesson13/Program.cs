61dfbf63a29340a921fe2f3a6ef6b66ce9510fa1
user <user@WINDOWS-OM6HICP>
2022-09-12 09:42:18 +0400

    Add project files.

---
+++ Lesson13/Program.cs
@@ -0,0 +1,30 @@
+﻿class Student
+{
+	public int Id { get; set; }
+	public string? Name { get; set; }
+	public string? Surname { get; set; }
+}
+
+class Program
+{
+	static void DoSomething(Action<Student> action)
+	{
+		var student = new Student();
+
+		action.Invoke(student);
+
+		Console.WriteLine(student.Id);
+		Console.WriteLine(student.Name);
+		Console.WriteLine(student.Surname);
+	}
+
+	static void Main()
+	{
+		DoSomething(s =>
+		{
+			s.Id = 1;
+			s.Name = "Nuran";
+			s.Surname = "Huseynova";
+		});
+	}
+}
